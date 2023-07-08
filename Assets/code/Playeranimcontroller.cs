5f083468535734aa5d88b78aa4333f25497d2504
upd123 <69945564+upd123@users.noreply.github.com>
2023-07-07 23:13:31 -0500

    house code

--- Assets/code/Playeranimcontroller.cs
+++ Assets/code/Playeranimcontroller.cs
@@ -17,8 +17,12 @@ public class Playeranimcontroller : MonoBehaviour
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
+<<<<<<< Updated upstream
         //Debug.Log(horizontalInput);
         //Debug.Log(verticalInput);
+=======
+     
+>>>>>>> Stashed changes
 
         if(horizontalInput != 0 || verticalInput != 0)
         {
